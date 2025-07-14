fb92b6c3371b38926672266d48478526502612f2
Michael Mueller <hello@michaelmueller.dev>
2025-07-14 22:55:51 +0200

    Add requirements, setup.py and environment files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(
+    name="art_dna",
+    version="0.0.1",
+    install_requires=requirements,
+    packages=find_packages(),
+)
